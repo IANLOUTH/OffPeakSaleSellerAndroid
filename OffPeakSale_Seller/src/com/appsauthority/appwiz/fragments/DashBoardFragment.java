1cafa661e0ae6f0ca9b9bdb00ac0980199f73be8
rbeh0002 <ramachandra.behra@synchronoss.com>
2015-12-13 12:25:19 +0530

    Dashboard DatePicker implemetation

--- OffPeakSale_Seller/src/com/appsauthority/appwiz/fragments/DashBoardFragment.java
+++ OffPeakSale_Seller/src/com/appsauthority/appwiz/fragments/DashBoardFragment.java
@@ -1,10 +1,11 @@
 package com.appsauthority.appwiz.fragments;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,50 +17,50 @@ import com.appsauthority.appwiz.utils.Constants;
 import com.appsauthority.appwiz.utils.HTTPHandler;
 import com.appsauthority.appwiz.utils.Helper;
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.zxing.oned.rss.FinderPattern;
 import com.offpeaksale.seller.R;
 
 import android.app.Activity;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.PorterDuff.Mode;
-import android.graphics.drawable.ClipDrawable;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.LayerDrawable;
-import android.view.Gravity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DashBoardFragment extends Fragment {
 
 	private View root;
 	Button ActivateQr;
 	private SharedPreferences spref;
-	TextView tvMerchantAccount, tvMerchantName1, tvMerchantUserIdText,
-			tvMerchantUserId, tvMerchantText, tvMerchantName,
-			tvMerchantLocationText, tvMerchantLocation, tvMerchantStartDateText,
-			tvMerchantStartDate, tvMerchantEndDateText,tvMerchantEndDate,tvRedeemedNumber, tvRedeemed, tvSoldNumber,
-			tvSold, tvProgressText;
+	TextView tvMerchantAccount, tvMerchantName1, tvMerchantUserIdText, tvMerchantUserId, tvMerchantText, tvMerchantName,
+			tvMerchantLocationText, tvMerchantLocation, tvMerchantStartDateText, tvMerchantStartDate,
+			tvMerchantEndDateText, tvMerchantEndDate, tvRedeemedNumber, tvRedeemed, tvSoldNumber, tvSold,
+			tvProgressText;
 	ProgressBar progressBar;
 	ProgressBar progressBarDownload;
 	RelativeLayout rootLayout;
-	
 
+	private DatePicker datePicker;
+	private Calendar calendar;
+	private int year, month, day;
+	TextView vDatePicker;
 	public DashBoardFragment(SlidingMenuActivity slidingMenuActivity) {
 	}
 
@@ -69,8 +70,7 @@ public class DashBoardFragment extends Fragment {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		root = inflater.inflate(R.layout.dahsboard_fragment, null);
 		spref = PreferenceManager.getDefaultSharedPreferences(getActivity());
 		initializeViews();
@@ -83,25 +83,19 @@ public class DashBoardFragment extends Fragment {
 
 		ActivateQr = (Button) root.findViewById(R.id.Activate);
 
-		tvMerchantAccount = (TextView) root
-				.findViewById(R.id.tvMerchantAccount);
+		tvMerchantAccount = (TextView) root.findViewById(R.id.tvMerchantAccount);
 		tvMerchantName1 = (TextView) root.findViewById(R.id.tvMerchantName1);
-		tvMerchantUserIdText = (TextView) root
-				.findViewById(R.id.tvMerchantUserIdText);
+		tvMerchantUserIdText = (TextView) root.findViewById(R.id.tvMerchantUserIdText);
 		tvMerchantUserId = (TextView) root.findViewById(R.id.tvMerchantUserId);
 		tvMerchantText = (TextView) root.findViewById(R.id.tvMerchantText);
 		tvMerchantName = (TextView) root.findViewById(R.id.tvMerchantName);
-		tvMerchantLocationText = (TextView) root
-				.findViewById(R.id.tvMerchantLocationText);
-		tvMerchantLocation = (TextView) root
-				.findViewById(R.id.tvMerchantLocation);
-		tvMerchantStartDateText = (TextView) root
-				.findViewById(R.id.tvMerchantStartDateText);
+		tvMerchantLocationText = (TextView) root.findViewById(R.id.tvMerchantLocationText);
+		tvMerchantLocation = (TextView) root.findViewById(R.id.tvMerchantLocation);
+		tvMerchantStartDateText = (TextView) root.findViewById(R.id.tvMerchantStartDateText);
 		tvMerchantStartDate = (TextView) root.findViewById(R.id.tvMerchantStartDate);
-		tvMerchantEndDateText = (TextView) root
-				.findViewById(R.id.tvMerchantEndDateText);
+		tvMerchantEndDateText = (TextView) root.findViewById(R.id.tvMerchantEndDateText);
 		tvMerchantEndDate = (TextView) root.findViewById(R.id.tvMerchantEndDate);
-		
+
 		tvRedeemedNumber = (TextView) root.findViewById(R.id.tvRedeemedNumber);
 		tvRedeemed = (TextView) root.findViewById(R.id.tvRedeemed);
 		tvSoldNumber = (TextView) root.findViewById(R.id.tvSoldNumber);
@@ -109,8 +103,7 @@ public class DashBoardFragment extends Fragment {
 		tvProgressText = (TextView) root.findViewById(R.id.tvProgressText);
 
 		progressBar = (ProgressBar) root.findViewById(R.id.progressBar);
-		progressBarDownload = (ProgressBar) root
-				.findViewById(R.id.progressBarDownload);
+		progressBarDownload = (ProgressBar) root.findViewById(R.id.progressBarDownload);
 
 		try {
 			ActivateQr.setTypeface(Helper.getSharedHelper().boldFont);
@@ -120,8 +113,7 @@ public class DashBoardFragment extends Fragment {
 			tvMerchantUserId.setTypeface(Helper.getSharedHelper().normalFont);
 			tvMerchantText.setTypeface(Helper.getSharedHelper().boldFont);
 			tvMerchantName.setTypeface(Helper.getSharedHelper().normalFont);
-			tvMerchantLocationText
-					.setTypeface(Helper.getSharedHelper().boldFont);
+			tvMerchantLocationText.setTypeface(Helper.getSharedHelper().boldFont);
 			tvMerchantLocation.setTypeface(Helper.getSharedHelper().normalFont);
 			tvMerchantStartDateText.setTypeface(Helper.getSharedHelper().boldFont);
 			tvMerchantStartDate.setTypeface(Helper.getSharedHelper().normalFont);
@@ -132,25 +124,19 @@ public class DashBoardFragment extends Fragment {
 			tvSoldNumber.setTypeface(Helper.getSharedHelper().boldFont);
 			tvSold.setTypeface(Helper.getSharedHelper().normalFont);
 			tvProgressText.setTypeface(Helper.getSharedHelper().normalFont);
-			ActivateQr.setBackgroundDrawable(Helper.getSharedHelper()
-					.getGradientDrawable(
-							Helper.getSharedHelper().reatiler.getHeaderColor()));
-			ActivateQr.setTextColor(Color.parseColor("#"
-					+ Helper.getSharedHelper().reatiler.getRetailerTextColor()));
-			
-			tvRedeemed.setTextColor(Color.parseColor("#"
-					+ Helper.getSharedHelper().reatiler.getHeaderColor()));
-			tvSold.setTextColor(Color.parseColor("#"
-					+ Helper.getSharedHelper().reatiler.getHeaderColor()));
-//			GradientDrawable bgShape = (GradientDrawable) progressBar
-//					.getProgressDrawable();
-//			bgShape.setColor(Color.parseColor("#"
-//					+ Helper.getSharedHelper().reatiler.getHeaderColor()));
-			progressBar.getProgressDrawable().setColorFilter(Color.parseColor("#"
-					+ Helper.getSharedHelper().reatiler.getHeaderColor()), Mode.SRC_IN);
-			
-		
-			
+			ActivateQr.setBackgroundDrawable(
+					Helper.getSharedHelper().getGradientDrawable(Helper.getSharedHelper().reatiler.getButton_color()));
+			ActivateQr.setTextColor(Color.parseColor("#" + Helper.getSharedHelper().reatiler.getRetailerTextColor()));
+
+			tvRedeemed.setTextColor(Color.parseColor("#" + Helper.getSharedHelper().reatiler.getHeaderColor()));
+			tvSold.setTextColor(Color.parseColor("#" + Helper.getSharedHelper().reatiler.getHeaderColor()));
+			// GradientDrawable bgShape = (GradientDrawable) progressBar
+			// .getProgressDrawable();
+			// bgShape.setColor(Color.parseColor("#"
+			// + Helper.getSharedHelper().reatiler.getHeaderColor()));
+			progressBar.getProgressDrawable().setColorFilter(
+					Color.parseColor("#" + Helper.getSharedHelper().reatiler.getHeaderColor()), Mode.SRC_IN);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -160,12 +146,37 @@ public class DashBoardFragment extends Fragment {
 			@Override
 			public void onClick(View arg0) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent(getActivity(),
-						RedeemVoucherActivity.class);
+				Intent intent = new Intent(getActivity(), RedeemVoucherActivity.class);
 				startActivity(intent);
 			}
 		});
 
+		calendar = Calendar.getInstance();
+		year = calendar.get(Calendar.YEAR);
+
+		month = calendar.get(Calendar.MONTH);
+		day = calendar.get(Calendar.DAY_OF_MONTH);
+		
+		populateSetDate( year, month, day, tvMerchantEndDate);
+
+		tvMerchantStartDate.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				setDate(v);
+				
+			}
+		});
+		tvMerchantEndDate.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+
+				setDate(v);
+				
+			}
+		});
 	}
 
 	@Override
@@ -178,6 +189,7 @@ public class DashBoardFragment extends Fragment {
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
+			progressBarDownload.setVisibility(View.VISIBLE);
 		}
 
 		@Override
@@ -189,9 +201,10 @@ public class DashBoardFragment extends Fragment {
 				String email = spref.getString(Constants.KEY_EMAIL, "");
 				param.put("retailerId", "MerchantA1");
 				param.put("email", email);
+				param.put("startDate", tvMerchantStartDate.getText().toString());
+				param.put("endDate", tvMerchantEndDate.getText().toString());
 				Log.e("Email: ", "Email  ::" + spref.getString("KEY_EMAIL", ""));
-				JSONObject jsonObject = HTTPHandler.defaultHandler().doPost(
-						Constants.URL_SELLER_ACCOUNT, param);
+				JSONObject jsonObject = HTTPHandler.defaultHandler().doPost(Constants.URL_SELLER_ACCOUNT, param);
 
 				return jsonObject;
 
@@ -214,8 +227,7 @@ public class DashBoardFragment extends Fragment {
 						if (errorCode != null && errorCode.equals("1")) {
 
 							Gson gson = new Gson();
-							UserDetailObject userDetail = gson.fromJson(
-									json.toString(), UserDetailObject.class);
+							UserDetailObject userDetail = gson.fromJson(json.toString(), UserDetailObject.class);
 							Profile userProfile = userDetail.userProfile;
 							setTextViews(userProfile);
 						}
@@ -241,16 +253,61 @@ public class DashBoardFragment extends Fragment {
 		// tvSold.setText(userProfile.company_name);
 		setProgress(userProfile.orders_redeemed, userProfile.orders_sold);
 	}
-	
-	void setProgress(String redeemed,String sold){
+
+	void setProgress(String redeemed, String sold) {
 		try {
-			int percentage = (int) ((Float.parseFloat(redeemed)/Float.parseFloat(sold))*100);
+			int percentage = (int) ((Float.parseFloat(redeemed) / Float.parseFloat(sold)) * 100);
 			progressBar.setProgress(percentage);
-			tvProgressText.setText(percentage+"%");
+			tvProgressText.setText(percentage + "%");
 			tvRedeemedNumber.setText(redeemed);
 			tvSoldNumber.setText(sold);
 		} catch (Exception e) {
 			// TODO: handle exception
-		} 
+		}
 	}
+
+	public void setDate(View view) {
+		vDatePicker=(TextView)view;
+		DialogFragment newFragment = new SelectDateFragment();
+		newFragment.show(getFragmentManager(), "DatePicker");
+	}
+
+	public class SelectDateFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			final Calendar calendar = Calendar.getInstance();
+			int yy = calendar.get(Calendar.YEAR);
+			int mm = calendar.get(Calendar.MONTH);
+			int dd = calendar.get(Calendar.DAY_OF_MONTH);
+			DatePickerDialog dialog=new DatePickerDialog(getActivity(), this, yy, mm, dd);
+//			dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.BLACK));
+			return dialog;
+		}
+
+		public void onDateSet(DatePicker view, int yy, int mm, int dd) {
+			populateSetDate(yy, mm+1, dd,vDatePicker);
+		}
+
+	}
+
+	public void populateSetDate(int year, int month, int day, TextView vPicker) {
+		String str_date = day+"-"+month+"-"+year;
+		 vPicker.setText(str_date);
+		 vPicker.setTextColor(Color.BLACK);
+			 new GetSellerInfo().execute();
+		/*Log.e("MONTH", str_date);
+		SimpleDateFormat readFormat = new SimpleDateFormat("yyyy-mm-dd",Locale.UK);
+		SimpleDateFormat  writeFormat = new SimpleDateFormat("dd MMM yyyy",Locale.UK);
+
+        try {
+         Date   date = readFormat.parse(str_date);
+         vPicker.setText(""+writeFormat.format(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }*/
+
+		
+	}
+
 }
